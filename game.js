2abb252ea28c56bde20b4659614262f66f513e81
Surendra Babu <surendrababubhupathi@gmail.com>
2024-02-12 18:17:49 +0530

    some changes

--- game.js
+++ game.js
@@ -1,15 +1,22 @@
+let balance = 0;
+
 function submit(){
     let user_input = document.getElementById("user_input");
     user_input = user_input.value
     let random_list = generateRandomList();
     if(checkNum(user_input,random_list)){
         document.getElementById("result").innerText="Congrats, You win...."
+        balance += 10;
     }
     else{
         document.getElementById("result").innerText="Sorry, You lost...."
+        balance -= 5;
     }
     
     document.getElementById("generatedList").innerText = random_list;
+    document.getElementById("balance").innerText = balance;
+
+    
     
 }
 
